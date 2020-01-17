99f37972379df1c3bee1f4a4c215d0a2ef3cce23
César Souza Liedke <cesarsl@gmail.com>
2020-01-17 12:25:00 -0300

    feat: better item parsing

--- scripts/item_to_prop.py
+++ scripts/item_to_prop.py
@@ -1,152 +1,101 @@
-import json
 import re
 
 import stealth
 
-PROPERTIES = [
-    "antique",
-    "assassin honed",
-    "balanced",
-    "bane",
-    "battle lust",
-    "berserk",
-    "blood drinker",
-    "bone breaker",
-    "brittle",
-    "casting focus",
-    "chaos damage",
-    "craft bonus",
-    "craft exceptional bonus",
-    "curse removal",
-    "cursed",
-    "damage",
-    "damage eater",
-    "damage increase",
-    "damage modifier",
-    "defense chance increase",
-    "dexterity bonus",
-    "durability",
-    "durability bonus",
-    "enhance potions",
-    "faster cast recovery",
-    "faster casting",
-    "hit area damage",
-    "hit chance increase",
-    "hit curse",
-    "hit dispel",
-    "hit fatigue",
-    "hit fireball",
-    "hit harm",
-    "hit life leech",
-    "hit lightning",
-    "hit lower attack",
-    "hit lower defense",
-    "hit magic arrow",
-    "hit mana drain",
-    "hit mana leech",
-    "hit point increase",
-    "hit point regeneration",
-    "hit stamina leech",
-    "intelligence bonus",
-    "last parry chance",
-    "lower ammo cost",
-    "lower mana cost",
-    "lower reagent cost",
-    "lower requirements",
-    "luck",
-    "mage armor",
-    "mage weapon",
-    "mana increase",
-    "mana phase",
-    "mana regeneration",
-    "massive",
-    "night sight",
-    "prized",
-    "random killer",
-    "random protection",
-    "random summoner",
-    "reactive paralyze",
-    "reflect physical damage",
-    "replenish charges",
-    "resist",
-    "resonance",
-    "self repair",
-    "skill bonus",
-    "slayer",
-    "soul charge",
-    "sparks",
-    "spell channeling",
-    "spell damage increase",
-    "spell focusing",
-    "splintering weapon",
-    "stamina increase",
-    "stamina regeneration",
-    "strength bonus",
-    "strength requirement",
-    "swarm",
-    "swing speed increase",
-    "unwieldy",
-    "use best weapon skill",
-    "velocity",
-    "ward removal",
-    "weapon speed",
-    "weight",
-    "weight reduction",
-    "wildfire removal",
-]
+from assets.metadata.cliloc import CLILOC, SKILLS_REQ, SLAYERS, HANDLE, NAMES, BOOLEANS
 
 
 class Item:
     def __init__(self, obj_id):
         self.obj_id = int(obj_id, 16)
-        self.cliloc = {}
+        self._tooltips = stealth.GetTooltipRec(self.obj_id)
+        self.properties = {}
 
-        with open("../assets/metadata/cliloc.json", "r", encoding="utf-8") as fpointer:
-            self.cliloc.update(json.load(fpointer))
-
-        for prop in PROPERTIES:
-            setattr(self, prop.replace(" ", "_"), None)
-
-        self.tooltips = stealth.GetTooltipRec(self.obj_id)
-
-        for tip in self.tooltips:
+        for tip in self._tooltips:
             cliloc_id = str(tip.get("Cliloc_ID"))
             cliloc_params = tip.get("Params")
-            prop_key = self.cliloc.get(cliloc_id).get("name").replace(" ", "_")
+            prop_key = re.sub(
+                re.compile(r"<.*?>"),
+                "",
+                CLILOC.get(cliloc_id).get("name").replace(" ", "_").casefold(),
+            )
             prop_params = []
             for param in cliloc_params:
-                prop_params.append(self.param_parser(param))
+                prop_params.append(self._param_parser(param))
 
-            if prop_key == "weapon_speed":
-                print(prop_key, prop_params)
-                setattr(self, prop_key, prop_params[0])
+            if cliloc_id == "1053099":
+                setattr(self, "material", prop_params[0])
+                setattr(self, "type", prop_params[1])
+            elif cliloc_id == "1072789":
+                setattr(self, "weight", prop_params[0])
+            elif int(cliloc_id) in BOOLEANS:
+                setattr(self, prop_key, True)
+            elif int(cliloc_id) in NAMES:
+                setattr(self, "name", prop_key.replace("_", " ").title())
+            elif int(cliloc_id) in SLAYERS:
+                setattr(self, "slayer", prop_key.split("_")[0])
+            elif int(cliloc_id) in SKILLS_REQ:
+                setattr(
+                    self,
+                    "skill_required",
+                    CLILOC.get(cliloc_id).get("name").split(": ")[1],
+                )
+            elif int(cliloc_id) in HANDLE:
+                setattr(
+                    self,
+                    "handling",
+                    CLILOC.get(cliloc_id).get("name").replace("_", " "),
+                )
+            elif cliloc_id == "1060639":
+                setattr(
+                    self,
+                    "durability",
+                    {"current": prop_params[0], "total": prop_params[1]},
+                )
             elif prop_key != "":
+                print(prop_key, prop_params)
                 if len(prop_params) == 1:
                     setattr(self, prop_key, prop_params[0])
 
-    def param_parser(self, param):
+        self._collect_props()
+
+    def _param_parser(self, param):
         if "#" in param:
-            return self.cliloc.get(param[1:]).get("name")
+            return CLILOC.get(param[1:]).get("name")
         if param.isdigit():
             return int(param)
-        if re.match(r"[0-9]+\.?[0-9]+s|[0-9]s", param):
-            return float(param.replace("s", ""))
+        if re.match(r"[0-9]+[\.\,]?[0-9]+s|[0-9]+s", param):
+            return float(param.replace("s", "").replace(",", "."))
         return str(param)
 
-    def props(self):
+    def _collect_props(self):
         props = {}
-        for prop in PROPERTIES:
-            prop_key = prop.replace(" ", "_")
-            props.update({prop_key: getattr(self, prop_key)})
+        attributes = [
+            attr
+            for attr in dir(self)
+            if not attr.startswith("_")
+            and getattr(self, attr) is not None
+            and attr != "all_props"
+            and attr != "properties"
+        ]
+        for attr in attributes:
+            props.update({attr: getattr(self, attr)})
+        self.properties = props
 
-        return props
+    @property
+    def all_props(self):
+        return self.properties
 
 
-equip_01 = Item("0x42E2D0E2")
-print(equip_01.weapon_speed)
-# equip_02 = Item("0x4070D4B7")
+# equip_02 = Item("0x415B7910")
 # print(equip_02.weapon_speed)
+# print(equip_02.type)
+# print(equip_02.material)
 
-equip_03 = Item("0x415B7910")
-print(equip_03.weapon_speed)
-print(equip_03.props())
+print(stealth.GetTooltipRec(0x40000108))
+equip_01 = Item("0x40000108")
+print(equip_01.all_props)
+
+print(stealth.GetTooltipRec(0x40000105))
+equip_02 = Item("0x40000105")
+print(equip_02.all_props)
